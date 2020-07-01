24cd1ce254477be692c1855dd64ef6c831eb9966
caiqueoliveir4 <caiquepark@hotmail.com>
2020-07-01 00:52:49 -0300

    Utilizando console.log e replace

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 var nome = "Caique de Oliveira ";
 var idade = 27 ;
 var localDeNascimento = "Bahia ";
-var frase = "Então quem nasce ma bahia é baiano"
+var frase = "Então quem nasce na bahia é baiano";
 alert(nome + "tem " + idade + " e nasceu no estado da " + localDeNascimento + frase);
+
+console.log(nome);
+console.log(localDeNascimento);
+console.log(idade);
+console.log(frase.replace(" baiano", " gaucho"));
+alert(frase.replace("baiano", "gaucho"))
