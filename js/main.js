5753220ff9b69c4965a92fce07c1b8cdb0865d95
caiqueoliveir4 <caiquepark@hotmail.com>
2020-07-02 12:00:48 -0300

    dando descrição a cada linha de codigo e utilizando laço for

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
+var count;
+for(count=0; count <=5;count++){
+    alert(count);
+}
+
+
+/* laço while
 var count = 0;
 while (count <=5){
     console.log(count);
     count = count + 1;
 }
-
-
+*/
+// condição 
 //var idade = prompt(" Qual a sua idade?: "); 
 //idade >=18?alert("Maior de idade"):alert("Menor de idade");
 
@@ -16,7 +23,7 @@ while (count <=5){
 */
 
 
-
+//dicionário
 /*var frutas = [{nome: "Maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
 console.log(frutas);
 alert(frutas[1].nome);
@@ -26,6 +33,8 @@ var fruta = {nome: "Maçã", cor: "vermelha"}
 console.log(fruta.nome);
 alert(fruta.cor);
 */
+
+//arrays e replace troca de nomes
 /*
 var nome = "Caique de Oliveira ";
 var idade = 27 ;
@@ -33,13 +42,14 @@ var localDeNascimento = "Bahia ";
 var frase = "Então quem nasce na bahia é baiano";
 alert(nome + "tem " + idade + " e nasceu no estado da " + localDeNascimento + frase);
 
+// ver no console os resutados
 console.log(nome);
 console.log(localDeNascimento);
 console.log(idade);
 console.log(frase.replace(" baiano", " gaucho"));
 alert(frase.replace("baiano", "gaucho"))
 */
-
+// declarando uma lista
 //var lista = ["Pêra", "Úva", "Maçã"];
 
 //lista.push("Goiaba")
